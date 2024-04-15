6f37ea8f6b5d9594c8b61657e1a022955f3bf602
akshayChavan3 <160063998+akshayChavan3@users.noreply.github.com>
2024-04-15 12:40:43 +0530

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,46 @@
+import math
+
+class Shape:
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def area(self):
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
+
+    def perimeter(self):
+        return self.side1 + self.side2 + self.side3
+
+# Example usage
+rectangle = Rectangle(8, 4)
+print("Rectangle Area:", rectangle.area())
+print("Rectangle Perimeter:", rectangle.perimeter())
+
+triangle = Triangle(3, 4, 5)
+print("Triangle Area:", triangle.area())
+print("Triangle Perimeter:", triangle.perimeter())
+
+
+
+
+
+
