9362f732630eab619dc97440c5f3d4637b970c42
akshayChavan3 <160063998+akshayChavan3@users.noreply.github.com>
2024-04-15 12:49:27 +0530

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,44 +1,32 @@
 import tkinter as tk
 from tkinter import PhotoImage
-from PIL import Image, ImageTk  # Import Image and ImageTk from PIL
-
-def create_window(image_file):
-    # Create a new window
-    window = tk.Toplevel(root)
-
-    # Display the image using a label
-    label = tk.Label(window, image=image_file)
-    label.pack()
 
 # Create the main window
 root = tk.Tk()
-root.title("Multiple Image Windows")
+root.title("Image Display")
 
-from PIL import Image, ImageTk
+# Load the image file (replace 'your_image_file.png' with your actual image file)
 
-from PIL import Image, ImageTk
+# image_path = r"C:\Users\sures\OneDrive\Desktop\istockphoto-1420676204-612x612.jpg"
 
-# List of image files
-image_files = [r"C:\Users\sures\PycharmProjects\ endangered animals\tiger-2535888_640.jpg",
-               r"C:\Users\sures\PycharmProjects\endangered animals\lion.jpg",
-               r"C:\Users\sures\PycharmProjects\ endangered animals\1698912318-8f8354e46b0a88ca473607040e6b6304f59b627fcf0131685a8a5cfc43db4278-d_640x360.jpg",
-               r"C:\Users\sures\PycharmProjects\ endangered animals\360_F_402953557_SWyol1myqjPhM5UcZOGPwifZWWTOYQlq.jpg"]
+background_image = PhotoImage(file= r"C:\Users\sures\OneDrive\Desktop\istockphoto-1420676204-612x612.jpg")
 
-# Create PhotoImage objects for each image
-images = []
-for file in image_files:
-    try:
-        img = Image.open(file)
-        images.append(ImageTk.PhotoImage(img))
-    except Exception as e:
-        print(f"Error loading image from {file}: {e}")
 
-# Print statements to check if images are loaded
-print("Number of images:", len(images))
+image_path = "C:\\Users\\sures\\OneDrive\\Desktop\\Asiatic-lion-Indias-conservation-success-story-scaled.webp"
 
-# Create a window for each image
-for image in images:
-    create_window(image)
+background_image = PhotoImage(file="C:\\Users\\sures\\OneDrive\\Desktop\\Asiatic-lion-Indias-conservation-success-story-scaled.webp")
+
+image_path = "C:\\Users\\sures\\OneDrive\\Desktop\\360_F_318477235_qtvHD7hAYa8V4Z7cIejqhqIhWlS9hQ6N.jpg"
+
+background_image = PhotoImage(file="C:\\Users\\sures\\OneDrive\\Desktop\\360_F_318477235_qtvHD7hAYa8V4Z7cIejqhqIhWlS9hQ6N.jpg")
+
+image_path = "C:\\Users\\sures\\OneDrive\\Desktop\\download (1).jpg"
+
+background_image = PhotoImage(file="C:\\Users\\sures\\OneDrive\\Desktop\\download (1).jpg")
+
+# Create a label to display the image
+image_label = tk.Label(root, image=background_image)
+image_label.pack()
 
 # Start the Tkinter event loop
 root.mainloop()
